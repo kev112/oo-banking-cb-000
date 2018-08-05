5b1811a9a2884b0f8c17b8a9c03b5c570c4bcb70
Tao <werewaswas@live.com>
2018-08-05 20:30:35 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,4 +13,16 @@ def transfer(to)
   Transfer.new(self, to)
 end
 
+def deposit(amount)
+  @balance += amount
+end
+
+def display_balance
+  "Your balance is $#{@balance}.")
+end
+
+def valid?
+  @balance == 0 || @status == 'closed' ? false : true
+end
+
 end
