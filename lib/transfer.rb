8145bac0c8ab1632dcfc30c52ea26ec300fbac76
Tao <werewaswas@live.com>
2018-08-05 21:05:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end
 
   def execute_transaction
-    if !valid?
+    if !self.valid?
       @status = 'rejected'
       "Transaction rejected. Please check your account balance."
     elsif status == 'complete'
